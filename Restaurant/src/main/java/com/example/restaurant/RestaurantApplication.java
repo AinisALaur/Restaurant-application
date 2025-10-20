83a6352a97197b8b3cb1ef6d8056e33f200dcc97
AinisALaur <ainislaurinavicius@gmail.com>
2025-10-20 19:44:54 +0300

    Code files

---
+++ Restaurant/src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -0,0 +1,19 @@
+package com.example.restaurant;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RestaurantApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RestaurantApplication.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
